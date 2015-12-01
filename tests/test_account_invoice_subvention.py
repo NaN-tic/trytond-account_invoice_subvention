1db8ddfc8a28a274c480c199697dc93464ddaff4
resteve <resteve@zikzakmedia.com>
2015-12-01 10:58:32 +0100

    Tests

--- tests/test_account_invoice_subvention.py
+++ tests/test_account_invoice_subvention.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
-# This file is part of account_invoice_subvention module for Tryton.
+# This file is part of the account_invoice_subvention module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.tests.test_tryton import test_view, test_depends
-import trytond.tests.test_tryton
 import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class AccountInvoiceSubventionTestCase(unittest.TestCase):
+class AccountInvoiceSubventionTestCase(ModuleTestCase):
     'Test Account Invoice Subvention module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('account_invoice_subvention')
-
-    def test0005views(self):
-        'Test views'
-        test_view('account_invoice_subvention')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'account_invoice_subvention'
 
 
 def suite():
